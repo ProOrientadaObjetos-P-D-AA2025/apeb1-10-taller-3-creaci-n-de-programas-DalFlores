892beda50f00035a9bcc5a18aaba7f1e0b04bf31
Dalton Flores <daltonflores@gmail.com>
2025-04-29 12:01:41 -0500

    Cambios Finales

--- Solucion_Codigo/Problema8/src/problema8/Problema_8Ejecutor.java
+++ Solucion_Codigo/Problema8/src/problema8/Problema_8Ejecutor.java
@@ -7,12 +7,12 @@ public class Problema_8Ejecutor {
         Random rand = new Random();
 
         String[] clientes = {"Gonzalo Gaviria", "Ana Chalacan", "Pablo Escobar"};
-        String[] bancos = {"Banco de Loja", "Banco del Pichincha", "Copmego"};
+        String[] bancos = {"Banco de Loja", "Banco del Pichincha", "Copmegos"};
         String nombreCliente = clientes[rand.nextInt(clientes.length)];
         String nombreBanco = bancos[rand.nextInt(bancos.length)];
         double valorCheque = 500 + rand.nextDouble() * 9500;  
 
-        Problema_8_ChequeBase cheque = new Problema_8_ChequeBase(nombreCliente, nombreBanco, valorCheque);
+        Problema_8Base cheque = new Problema_8Base(nombreCliente, nombreBanco, valorCheque);
 
         System.out.println(cheque);
     }
